ecfc2c3ef869e92b5ee87bacb2e5f1a6d9de2444
Olanrewaju <olanrewaju@seamlesshr.com>
2022-10-31 17:15:18 +0100

    included the condition that allows players restart the game and returned appropriate message when they are done playing

--- app/Console/Commands/RockPaperScissors.php
+++ app/Console/Commands/RockPaperScissors.php
@@ -41,51 +41,9 @@ class RockPaperScissors extends Command
      */
     public function handle()
     {   
-        $user_one = $this->ask('Enter your preferred username');
-        $game_options = [GameChoiceEnum::ROCK->value, GameChoiceEnum::SCISSORS->value, GameChoiceEnum::PAPER->value];
-        $game_setting = $this->choice("Welcome {$user_one } !, How will you like to play Rock-Paper-Scissors? ", 
-                                ['Other Human', 'Computer'], $defaultIndex = 1, $maxAttempts = null,
-                                $allowMultipleSelections = false);
+        $this->startRockPaperScissorsGame();
 
-        switch($game_setting) {
-            case 'Other Human' :
-                $this->info('The game will be played with another human. To proceed enter second user preferred username');
-                
-                $user_two = $this->ask('Enter your preferred username');
-                
-                $user_one_game_choice =  $this->choice("{$user_one } !, Select game option .. ", 
-                                $game_options, $defaultIndex = rand(0,2), $maxAttempts = null,
-                                $allowMultipleSelections = false);
-
-                $this->info("{$user_one } selection ok, waiting for {$user_two} ...");
-
-                $user_two_game_choice =  $this->choice("{$user_two } !, Select game option .. ", 
-                                $game_options, $defaultIndex = rand(0,2), $maxAttempts = null,
-                                $allowMultipleSelections = false);
-
-                $game_result = $this->switch_result_outcome($user_one_game_choice, $user_two_game_choice, $user_one);
-            
-                $this->info($game_result);
-        
-                return;
-            break;
-            default:
-            $this->info('The game will be played with a computer. ');
-        
-            $user_one_game_choice =  $this->choice("{$user_one } !, Select game option .. ", 
-                            $game_options, $defaultIndex = rand(0,2), $maxAttempts = null,
-                            $allowMultipleSelections = false);
-            
-            $computer_choice = $game_options[array_rand($game_options, 1)];
-            
-            $game_result = $this->switch_result_outcome($user_one_game_choice, $computer_choice, $user_one);
-            
-            $this->info($game_result);
-
-            return;
-        }
-
-        return;
+        return 0;
     }
 
     private function switch_result_outcome($user_one_value, $user_two_value, $username) : string
@@ -105,5 +63,69 @@ class RockPaperScissors extends Command
         }
 
         return $final_result;
+    }
+
+    private function startRockPaperScissorsGame() : void
+    {
+        $user_one = $this->ask('Enter your preferred username') ?? 'User1';
+        $game_options = [GameChoiceEnum::ROCK->value, GameChoiceEnum::SCISSORS->value, GameChoiceEnum::PAPER->value];
+        $game_setting = $this->choice("Welcome {$user_one } !, How will you like to play Rock-Paper-Scissors? ", 
+                                ['Other Human', 'Computer'], $defaultIndex = 1, $maxAttempts = null,
+                                $allowMultipleSelections = false);
+
+        switch($game_setting) {
+            case 'Other Human' :
+                $this->info('The game will be played with another human. To proceed enter second user preferred username');
+                
+                $user_two = $this->ask('Enter your preferred username') ?? 'User2';
+                
+                $user_one_game_choice =  $this->choice("{$user_one } !, Select game option .. ", 
+                                $game_options, $defaultIndex = rand(0,2), $maxAttempts = null,
+                                $allowMultipleSelections = false);
+
+                $this->info("{$user_one } selection ok, waiting for {$user_two} ...");
+
+                $user_two_game_choice =  $this->choice("{$user_two } !, Select game option .. ", 
+                                $game_options, $defaultIndex = rand(0,2), $maxAttempts = null,
+                                $allowMultipleSelections = false);
+
+                $game_result = $this->switch_result_outcome($user_one_game_choice, $user_two_game_choice, $user_one);
+            
+                $this->info($game_result);
+                
+                $this->restartGame();
+
+                return;
+            break;
+            default:
+            $this->info('The game will be played with a computer. ');
+        
+            $user_one_game_choice =  $this->choice("{$user_one } !, Select game option .. ", 
+                            $game_options, $defaultIndex = rand(0,2), $maxAttempts = null,
+                            $allowMultipleSelections = false);
+            
+            $computer_choice = $game_options[array_rand($game_options, 1)];
+            
+            $game_result = $this->switch_result_outcome($user_one_game_choice, $computer_choice, $user_one);
+            
+            $this->info($game_result);
+
+            $this->restartGame();
+
+            return;
+        }
+    }
+
+    public function restartGame() :void
+    {
+        $play_new_game = $this->choice("Will you like to play Rock-Paper-Scissors again? ", 
+            ['No', 'Yes'], $defaultIndex = 1, $maxAttempts = null,
+            $allowMultipleSelections = false);
+            if($play_new_game == 'Yes') {
+                $this->startRockPaperScissorsGame();
+            }else{
+                $this->info('Thank you for playing !');
+                return;
+            }
     }
 }
